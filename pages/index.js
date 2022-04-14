a89a89736e740089245d7b35af037c8f2fc350dc
John William Davis <jwdavisdeveloper@gmail.com>
2022-04-14 12:39:11 -0700

    Setup submit form and initial form state

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,57 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import { useState } from 'react'
 
 const Home = () => {
+  const [username, setUserName] = useState('')
+  const [programmer, setProgrammer] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username)
+    setUserName('')
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>My TailwindCSS / Next.js Starter</title>
+        <title>GitHub Inspirations</title>
         <link rel="icon" href="/surfing.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          My&nbsp;
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js&nbsp;
-          </a>
-          &&nbsp;
-          <a className="text-blue-600" href="https://tailwind.css">
-            TailwindCSS&nbsp;
-          </a>
-          Starter
+        <h1 className="text-4xl font-bold md:text-6xl">
+          My GitHub Inspirations
         </h1>
+        <h2 className="mt-3 text-xl capitalize md:text-3xl">
+          programmers whose work I admire
+        </h2>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <section className="mt-6">
+          <p className="mt-3 text-xl">
+            <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+              // enter a github username below
+            </code>
+          </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <form className="mt-3" onSubmit={handleSubmit}>
+            <div className="flex flex-col">
+              <label htmlFor="username">username</label>
+              <input
+                type="text"
+                name="username"
+                className="my-3 outline"
+                onChange={(e) => setUserName(e.target.value)}
+                value={username}
+              />
+            </div>
+            <button
+              type="submit"
+              className="px-3 py-2 text-white bg-blue-400 rounded-md"
+            >
+              Send UserName
+            </button>
+          </form>
+        </section>
       </main>
       <Footer />
     </div>
